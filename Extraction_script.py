8db726538657863c3ebe6d571b8ba4d08bfc4553
Sabab080 <nsabab08@gmail.com>
2024-04-20 14:37:44 +0600

    script_updated

--- Extraction_script.py
+++ Extraction_script.py
@@ -10,54 +10,25 @@ from airflow.operators.python import PythonOperator
 
 
 # Define MySQL connection parameters
-source_host = '157.230.43.223'
-source_port = '3306'
-source_user = 'noor'
-source_password = 'OkOGf0iIOnwK56hd'
-source_schema = 'zingpay'
+source_host = 'your host IP'
+source_port = 'port'
+source_user = 'user'
+source_password = 'pass'
+source_schema = 'source_database_name'
 
 # Target MySQL connection parameters
-target_host = '127.0.0.1'
-target_port = 3306
-target_user = 'test'
-target_password = 'test1234'
-target_schema = 'zingpay_target'
-target_table = 'daily_sales_table'
+target_host = 'target IP'
+target_port = 'port'
+target_user = 'target_user'
+target_password = 'target_pass'
+target_schema = 'target_database_name'
+target_table = 'target_table'
 
 
 
 # Define SQL query for data extraction
 extract_query = """
-    Select current_date() as 'insertion_date', 
-	date(transaction.datetime) 'date_value',
-    app_user.full_name 'customer_name',
-    app_user.username 'username',
-    app_user.business_name 'business_name',
-    app_user.parent_id 'parent_id',
-    (select concat(full_name,"-",username) from app_user au2 where account_id = app_user.parent_id) 'parent_name_id', 
-    organization_branch.branch_name 'branch_name',
-    zingpay_transaction_type.description 'transaction_type_id',
-    ifnull(sum(( select transaction.amount where transaction_type_id='1')), 0) as debit,
-    ifnull(sum(( select transaction.amount where transaction_type_id='2')),0) as credit,
-    Count(( select transaction.amount where transaction_type_id='1')) as debit_count,
-    count(( select transaction.amount where transaction_type_id='2')) as credit_count,
-    ifnull(sum(( select transaction.amount where transaction_type_id='2')),0) - ifnull(sum(( select transaction.amount where transaction_type_id='1')), 0) as 'net'    
-    from zingpay.transaction 
-    left join zingpay.app_user on app_user.account_id=transaction.account_id
-    left join zingpay.zingpay_transaction_type on zingpay_transaction_type.zingpay_transaction_type_id=transaction.zingpay_transaction_type_id
-    left join transaction_status on transaction_status.id=transaction.transaction_status_id
-    left join organization_branch on app_user.branch_id=organization_branch.branch_id
-    where  transaction.transaction_status_id=1
-        and date(datetime)>=current_date()-100
-    Group by 
-    date(transaction.datetime),
-    app_user.full_name,
-    app_user.username,
-    app_user.business_name,
-    app_user.parent_id,
-    (select concat(full_name,"-",username) from app_user au2 where account_id = app_user.parent_id),
-    organization_branch.branch_name,
-    zingpay_transaction_type.description;
+        # Your query logic
 """
 
 # Define function to extract data from source MySQL database
